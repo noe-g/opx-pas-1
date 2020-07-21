14c6a82d4ecbc4fdf2cd086e12339a4baac0a5ce
Giraud <noegiraud@aol.com>
2020-07-21 15:15:48 +0200

    add log

--- src/pas_entity.c
+++ src/pas_entity.c
@@ -140,6 +140,7 @@ bool dn_cache_init_entity(void)
     for (i = 0; i < ARRAY_SIZE(entity_type_tbl); ++i) {
         for (slot = 1,
                  n = sdi_entity_count_get(entity_type_tbl[i].sdi_entity_type);
+        		printf("entity count:%d\n");
              n;
              --n, ++slot
              ) {
